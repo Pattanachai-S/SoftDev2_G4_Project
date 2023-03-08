37e32dccfc3218c46c94e0568fad0ef0379d5663
Pattanachai-S <69448138+Pattanachai-S@users.noreply.github.com>
2023-03-08 22:47:26 +0700

    created database

--- myApp/webApp/models.py
+++ myApp/webApp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    surname = models.CharField(max_length=200)
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+
+class Subject(models.Model):
+    subject_ID = models.IntegerField()
+    name = models.CharField(max_length=200)
+    mid_term = models.DateTimeField('mid term')
+    final = models.DateTimeField('final')
+
+class Section(models.Model):
+    subject_ID = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    sec_num = models.IntegerField()
+    day = models.CharField(max_length=200)
+    time = models.CharField(max_length=200)
+    teacher = models.CharField(max_length=200)
+
+
