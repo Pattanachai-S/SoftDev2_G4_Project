ef7ef9b6a68d467de04c59472d241d4064ec6765
Pattanachai-S <69448138+Pattanachai-S@users.noreply.github.com>
2023-03-09 01:24:26 +0700

    bind model to admin page

--- mysite/helpApp/admin.py
+++ mysite/helpApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Subject
+from .models import Section
+
+admin.site.register(Subject)
+admin.site.register(Section)
