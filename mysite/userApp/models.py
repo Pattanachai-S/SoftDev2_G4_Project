c7c23cb18c4a4b47c06900f85d405756c2cea4ae
Pattanachai-S <69448138+Pattanachai-S@users.noreply.github.com>
2023-03-08 23:15:30 +0700

    create table for userApp

--- mysite/userApp/models.py
+++ mysite/userApp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    surname = models.CharField(max_length=200)
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
