6dd664fa547986cbe49096620471d2b501945a9d
Pattanachai-S <69448138+Pattanachai-S@users.noreply.github.com>
2023-03-08 23:17:19 +0700

    create table for helpApp

--- mysite/helpApp/models.py
+++ mysite/helpApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Subject(models.Model):
+    subject_ID = models.IntegerField()
+    name = models.CharField(max_length=200)
+    mid_term = models.DateTimeField('mid term')
+    final = models.DateTimeField('final')
+
+class Section(models.Model):
+    subject_ID = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    sec_num = models.IntegerField()
+    day = models.CharField(max_length=200)
+    time = models.CharField(max_length=200)
+    teacher = models.CharField(max_length=200)
