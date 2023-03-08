8b1e465072cfa6f4eadea457f5898d8115927eb6
Pattanachai-S <69448138+Pattanachai-S@users.noreply.github.com>
2023-03-09 00:11:14 +0700

    create test post for register page

--- mysite/userApp/tests.py
+++ mysite/userApp/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class RegisterTest(TestCase):
+
+    register_url = '/users/register'
+
+    def test_uses_register_template(self):
+        response = self.client.get(self.register_url)
+        self.assertTemplateUsed(response, 'userApp/register.html')
+
+
+    def test_can_register(self):
+        response = self.client.post(self.register_url, data={
+            'username': 'user123456',
+            'password1': 'password123456',
+            'password2': 'password123456'
+            })
+        self.assertIn('user123456', response.content.decode())  # test can post request
+
+        " Test database here "
+
+
+
+    def test_register(self):
+        pass
+
