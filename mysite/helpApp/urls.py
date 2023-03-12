2abef41b0d40ea548e4dbf706d02c18c6aa1f675
wannipaa <s6401012630116@email.kmutnb.ac.th>
2023-03-12 23:32:52 +0700

    update UI

--- mysite/helpApp/urls.py
+++ mysite/helpApp/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('studytimetable', views.studyTimetable, name='study'),
     path('examtimetable', views.examTimetable, name='exam'),
     path('checksubject', views.checkSubject, name='check'),
-    
+    path('settingtimetable', views.settingtable, name='table'),
+    path('<int:subject_ID>/setting/', views.subjectsetting, name='subjectsetting'),
 ]
--- mysite/helpApp/views.py
+++ mysite/helpApp/views.py
@@ -1,5 +1,6 @@
-from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Section, Subject
+from django.db.models import Q
 
 def studyTimetable(request):
     return render(request, 'helpApp/studytimetable.html')
@@ -8,4 +9,28 @@ def examTimetable(request):
     return render(request, 'helpApp/examtimetable.html')
 
 def checkSubject(request):
-    return render(request, 'helpApp/checksubject.html')
+    if 'q' in request.GET:
+        q = request.GET.get('q')
+        #subject = Subject.objects.filter(subject_ID__icontains=q)
+        multi_q = Q(Q(subject_ID__icontains=q) | Q(name__icontains=q))
+        subject = Subject.objects.filter(multi_q)
+    else:
+        subject = Subject.objects.all()
+    context = {'subject':subject}
+    return render(request, 'helpApp/checksubject.html', context)
+
+def settingtable(request):
+    if 'q' in request.GET:
+        q = request.GET.get('q')
+        #subject = Subject.objects.filter(subject_ID__icontains=q)
+        multi_q = Q(Q(subject_ID__icontains=q) | Q(name__icontains=q))
+        subject = Subject.objects.filter(multi_q)
+    else:
+        subject = Subject.objects.all()
+    context = {'subject':subject}
+    return render(request, 'helpApp/settingtable.html', context)
+
+def subjectsetting(request, subject_ID):
+    subject = get_object_or_404(Subject, pk=subject_ID)
+    return render(request, 'helpApp/selectsubject.html', {'subject':subject})
+
