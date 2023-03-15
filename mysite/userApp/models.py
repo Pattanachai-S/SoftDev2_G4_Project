655be4d2aba431a0684d6fe5783e3268e12216eb
Pattanachai-S <69448138+Pattanachai-S@users.noreply.github.com>
2023-03-15 23:26:17 +0700

    create user subject

--- mysite/userApp/models.py
+++ mysite/userApp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from helpApp.models import Subject
 
 # Create your models here.
 class User(models.Model):
@@ -6,3 +7,9 @@ class User(models.Model):
     surname = models.CharField(max_length=200)
     username = models.CharField(max_length=200)
     password = models.CharField(max_length=200)
+
+class User_subject(models.Model):
+    # auth.user.id, helpApp_subject, can_submit
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    can_submit = models.BooleanField(default=False)
