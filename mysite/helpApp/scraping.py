781c3ff5f9108e0e3c02b08bfe62bee1f946fe77
Pattanachai-S <69448138+Pattanachai-S@users.noreply.github.com>
2023-03-16 11:21:11 +0700

    sort scraping sec

--- mysite/helpApp/scraping.py
+++ mysite/helpApp/scraping.py
@@ -6,8 +6,11 @@ import sqlite3
 
 class Scraping():
     
-    def __init__(self):
+    def __init__(self) -> None:
         self.dir_path = os.path.dirname(os.path.realpath(__file__))
+
+    def scrap_subject(self):
+        """For Subject"""
         os.chdir(self.dir_path)
         file = "dataQuerySelector.html"
 
@@ -20,7 +23,6 @@ class Scraping():
         # find all subject
         table = soup.find('table', attrs={'align': 'center', 'border': '1', 'cellpadding': '2', 'cellspacing': '0', 'width': '100%', 'bordercolor': '#CCCCCC'})
 
-
         self.subject_list = []
         # Loop through the tr and td elements of the table
         for row in table.find_all('tr'):
@@ -31,8 +33,15 @@ class Scraping():
                 subject.append(text.replace('\n\t\t', '').replace('  ', ' '))
             self.subject_list.append(subject)
 
+
     def show_data(self):
         for row in self.subject_list:
+            print("---------------")
+            for cell in row:
+                print(cell)
+
+    def show_data_sec(self):
+        for row in self.section_list:
             print("---------------")
             for cell in row:
                 print(cell)
@@ -40,7 +49,6 @@ class Scraping():
     def save_to_database(self):
         os.chdir(self.dir_path)
         os.chdir("../")
-        print(os.getcwd())
         conn = sqlite3.connect('db.sqlite3')
         # Create a cursor object to interact with the database
         c = conn.cursor()
@@ -56,6 +64,7 @@ class Scraping():
         conn.commit()  # Save changes to the database
         conn.close()  # Close the connection
         
+    # not use
     def convert_to_date_format(self, date):
         print(date)
         if date != " ":
@@ -68,12 +77,147 @@ class Scraping():
         else:
             return " "
     
+    def clear_table(self):
+        import sqlite3
+
+        # Create a connection to the database
+        os.chdir(self.dir_path)
+        os.chdir("../")
+        conn = sqlite3.connect('db.sqlite3')      
+        c = conn.cursor()  # Create a cursor object to interact with the database
+        
+        c.execute("DELETE FROM helpApp_subject") # Execute a DELETE statement to clear all data from the table
+
+        conn.commit() # Save changes to the database     
+        conn.close() # Close the connection
+
+    def scrap_section(self):       
+        """For Section"""
+
+        os.chdir(self.dir_path)
+        file_Sec = "dataQuerySelectorSec.html"
+        with open(file_Sec, 'r', encoding="cp874") as file_Sec:
+            html_content = file_Sec.read()
+        soup = BeautifulSoup(html_content, 'html.parser')
+
+        subject_name_list = soup.find_all('tr', attrs={'bgcolor': '#EA98FF'})
+        section_list =  soup.find_all('tr', attrs={'bgcolor': '#F1F1FD'})
+
+        # find all subject id
+        subject_id_list = []
+        for text in subject_name_list:
+            subject = text.find("b")
+            subject_id_list.append(subject.get_text()[0:9])
+
+        sec_list = []
+        for text in section_list:
+            sec_number = text.find('td',attrs={'valign':'top'}).get_text()
+            sec_info = text.find('tbody')
+            sec_day = sec_info.find('td',attrs={'width':'7%'}).get_text()
+            sec_time = sec_info.find('td',attrs={'width':'25%'}).get_text()
+            sec_teacher = sec_info.find('td',attrs={'width':'46%'}).get_text().replace("\xa0","")
+            sec_list.append([sec_number,sec_day,sec_time,sec_teacher])
+
+
+        # find subject id and all sec
+        subject_and_sec = []
+        sub = soup.find_all('tr')
+        for text in sub:
+            t = text.find("b")
+            if t != None:
+                # print(t.get_text())
+                s = t.get_text().split(" ")
+                subject_id = s[0]
+                
+
+            sec_number = text.find('td',attrs={'valign':'top'})
+            if sec_number != None:
+                # print(sec_number.get_text())
+                sec_number = sec_number.get_text()
+               
+            sec_info = text.find('tbody')
+            if sec_info != None:
+                sec_day = sec_info.find('td',attrs={'width':'7%'})
+                if sec_day != None:
+                    # print(sec_day.get_text())
+                    sec_day = sec_day.get_text()
+
+                sec_time = sec_info.find('td',attrs={'width':'25%'})
+                if sec_time != None:
+                    # print(sec_time.get_text())
+                    sec_time = sec_time.get_text()
+
+                sec_teacher = sec_info.find('td',attrs={'width':'46%'})
+                if sec_teacher != None:
+                    # print(sec_teacher.get_text())
+                    sec_teacher = sec_teacher.get_text().replace("\xa0","")
+
+                sec_info = [sec_number,sec_day,sec_time,sec_teacher]
+                subject_and_sec.append([subject_id,sec_info])
+        subject_and_sec.pop(0)  # remove text คณะ....
+        self.subject_and_sec = subject_and_sec
+
+    def show_data_sec(self):
+        for sns in self.subject_and_sec:
+            print(sns)
+            pass
+
+
+    #     print(subject_id_list)
+    #     print(sec_list)
+
+    #     sec_list_index = 0
+    #     subject_and_sec = []
+    #     for subject_id in subject_id_list:
+    #         sec = []     
+    #         while(self.next_sec(sec_list[sec_list_index])):
+    #             sec.append(sec_list[sec_list_index])
+    #             sec_list_index += 1
+    #         subject_and_sec.append([subject_id,sec])
+                
+    # def next_sec(self):
+
+
+    def save_sec_to_database(self):
+        # connect to the database
+        os.chdir(self.dir_path)
+        os.chdir("../")
+        conn = sqlite3.connect('db.sqlite3')
+        cur = conn.cursor() # create a cursor object
+
+        # # execute a SELECT statement to search for records
+        # cur.execute("SELECT subject_id FROM helpApp_subject")    
+        # results = cur.fetchall()  # fetch the results
+
+        for sns in self.subject_and_sec:
+            subject_id = sns[0]
+            sec_number = sns[1][0]
+            sec_day = sns[1][1]
+            sec_time = sns[1][2]
+            sec_teacher = sns[1][3]
+            cur.execute("INSERT INTO helpApp_section (subject_ID, sec_num, day, time, teacher) VALUES (?, ?, ?, ?, ?)", (subject_id,sec_number,sec_day,sec_time,sec_teacher))
+
+        # close the cursor and connection
+        cur.close()
+        conn.close()
+
+
+
+
+
+
+
+        
 
     
 if __name__ == "__main__":
     scraping = Scraping()
-    scraping.show_data()
-    scraping.save_to_database()
+    # scraping.scrap_subject()
+    # scraping.show_data()
+    # scraping.save_to_database()
+    scraping.scrap_section()
+    # scraping.show_data_sec() 
+    scraping.save_sec_to_database()
     
 
 
