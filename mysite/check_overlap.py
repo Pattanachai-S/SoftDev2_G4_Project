36a05425270ccddfc955960aee5a399c7dd80a39
Pattanachai-S <69448138+Pattanachai-S@users.noreply.github.com>
2023-03-19 05:12:49 +0700

    check exam and study overlap

--- mysite/check_overlap.py
+++ mysite/check_overlap.py
@@ -1,9 +1,6 @@
 
 import os
 import django
-dir_path = os.path.dirname(os.path.realpath(__file__))
-os.chdir(dir_path)
-os.chdir("../")
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 django.setup()
 
@@ -11,7 +8,7 @@ from datetime import datetime, time
 import os 
 import sqlite3
 from django.db import models
-from models import Subject,Section
+from helpApp.models import Subject,Section
 
 
 
@@ -67,19 +64,55 @@ class Overlap():
         else:
             return True
         
+    def is_both_have_exam(self, first_sub, second_sub):
+        if len(first_sub) >= 2 and len(second_sub) >= 2:  # have date and time
+            return True
+        else:
+            return False
+
     def is_subject_overlap(self, first_sub, second_sub):
         """Return True if overlap
             Input: [['subject_code','sec']]"""
         
         code1 = first_sub[0]
         code2 = second_sub[0]
+        sec_num1 = first_sub[1]
+        sec_num2 = second_sub[1]
 
         # Find exam time in database
         mid_term_col = "mid_term"
         final_col = "final"
         # 1st subject
-        sub1 = Subject.objects.filter(subject_ID=code1).values(mid_term_col, final_col)
-        print(sub1)
+        sub1 = Subject.objects.filter(subject_ID=code1)
+        sub1 = sub1[0] # Get 1st query
+        mid1 = sub1.mid_term.split(" ")  # พฤ. 30/3/2566 13:00-16:00
+        final1 = sub1.final.split(" ")
+        # 2nd subject
+        sub2 = Subject.objects.filter(subject_ID=code2)
+        sub2 = sub2[0] # Get 1st query
+        mid2 = sub2.mid_term.split(" ")  #พฤ. 30/3/2566 13:00-16:00
+        final2 = sub2.final.split(" ")
+
+        # Check exam date
+        if (self.is_both_have_exam(mid1,mid2)):  # midterm
+            if (self.is_overlap(mid1[1:],mid2[1:])): return True
+        if (self.is_both_have_exam(final1,final2)):  # final  
+            if (self.is_overlap(final1[1:],final2[1:])): return True
+
+        # Find study day-time in database
+        sec1 = Section.objects.filter(subject_ID = sub1, sec_num = sec_num1)
+        sec2 = Section.objects.filter(subject_ID = sub2, sec_num = sec_num2)
+        # loop for subject have 2 day study case
+        for sec_sub1 in sec1:
+            for sec_sub2 in sec2:
+                if (self.is_overlap([sec_sub1.day, sec_sub1.time], [sec_sub2.day, sec_sub2.time])):
+                    return True  # If have any overlap will return True
+                
+        # return False after check exam date-time and study day-time is do not overlap
+        return False
+
+
+
 
             
 
@@ -88,7 +121,7 @@ class Overlap():
 if __name__ == "__main__":
     c = Overlap()
     # all test is Ture or Overlap
-    print(c.is_time_overlap("9:00-12:00","9:00-12:00"))
-    print(c.is_time_overlap("13:00-15:00","12:00-14:00"))
+    # print(c.is_time_overlap("9:00-12:00","9:00-12:00"))
+    # print(c.is_time_overlap("13:00-15:00","12:00-14:00"))
 
     print(c.is_subject_overlap(['010113138','S.1'],['010123124','S.1']))
