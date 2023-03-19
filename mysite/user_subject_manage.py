b5fb1453ff207aa25406eae44a1b911385d4ffa7
Pattanachai-S <69448138+Pattanachai-S@users.noreply.github.com>
2023-03-19 18:48:53 +0700

    create remove subject for user

--- mysite/user_subject_manage.py
+++ mysite/user_subject_manage.py
@@ -15,8 +15,8 @@ class subject_manage():
         # App section_subject to database for user
 
         # Get user_id and sec_id
-        user = User.objects.filter(username = user_name).first()
-        sub = Subject.objects.filter(subject_ID = subject).first()
+        user = User.objects.get(username = user_name)
+        sub = Subject.objects.get(subject_ID = subject)
         sec_list = Section.objects.filter(subject_ID = sub, sec_num = section)
 
         # Loop for subject have 2 day study case
@@ -24,9 +24,19 @@ class subject_manage():
             user_subject = User_subject(user_id = user, section = sec)  # Create object
             user_subject.save()  # Add to database
 
-    def remove_subject():
+    def remove_subject(self, user_name, subject, section):
+        """ Input: 'username', 'sunject_code', 'section_subject' """
         # Remove section_subject in database for user
-        pass
+        # Get user_id and sec_id
+        user = User.objects.get(username = user_name)
+        sub = Subject.objects.get(subject_ID = subject)
+        sec_list = Section.objects.filter(subject_ID = sub, sec_num = section)
+
+        # Loop for subject have 2 day study case
+        for sec in sec_list:   
+            user_subject = User_subject.objects.filter(user_id = user, section = sec)  # Get object
+            user_subject.delete()  # remove from database
+
 
     def check_section_can_submit():
         # Check all Section what user choose in database can submit
