b520351e6a6776c884fcd780aa137e5c2810e675
wannipaa <s6401012630116@email.kmutnb.ac.th>
2023-03-20 15:13:42 +0700

    fix a little

--- mysite/helpApp/views.py
+++ mysite/helpApp/views.py
@@ -6,9 +6,24 @@ from django.db.models import Q
 
 def studyTimetable(request):
     if request.user.is_authenticated:
-        course = User_subject.objects.filter(user_id=request.user)
-        time = ['8:00', '9:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00']
-        return render(request, 'helpApp/studytimetable.html', {'course':course, 'time':time})
+        courses = User_subject.objects.filter(user_id=request.user)
+
+        time_used = {'M': [], 'T': [], 'W': [], 'H': [], 'F': []}
+        times = ['8:00', '9:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00']
+        for course in courses:
+            day = course.section.day
+            start_t = course.section.start_time
+            end_t = course.section.end_time
+            keep = False
+            for time in times:
+                if start_t == time:
+                    keep = True
+                if end_t == time:
+                    keep = False
+                if (keep): 
+                    time_used[day].append(time)
+                
+        return render(request, 'helpApp/studytimetable.html', {'course':courses, 'time':times, 'time_usedd':time_used})
     else:
         return render(request, 'helpApp/studytimetable.html')
 
