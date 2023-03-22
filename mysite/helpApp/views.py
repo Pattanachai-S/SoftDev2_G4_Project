8eb9c68ff3af007f4c42cd15e7179894acae22dc
wannipaa <s6401012630116@email.kmutnb.ac.th>
2023-03-23 01:06:44 +0700

    fix study timetable and edit

--- mysite/helpApp/views.py
+++ mysite/helpApp/views.py
@@ -50,14 +50,14 @@ def checkSubject(request):
     return render(request, 'helpApp/checksubject.html', context)
 
 def settingtable(request):
+    courses = User_subject.objects.filter(user_id=request.user)
     if 'search-subject' in request.GET:
         q = request.GET.get('search-subject')
-        #subject = Subject.objects.filter(subject_ID__icontains=q)
         multi_q = Q(Q(subject_ID__icontains=q) | Q(name__icontains=q))
-        subject = Subject.objects.filter(multi_q)
+        subject = Subject.objects.filter(multi_q).exclude(section__in=courses.values_list('section', flat=True))
     else:
-        subject = Subject.objects.all()
-    return render(request, 'helpApp/settingtable.html', {'subject':subject})
+        subject = Subject.objects.exclude(section__in=courses.values_list('section', flat=True))
+    return render(request, 'helpApp/settingtable.html', {'subject':subject, 'course':courses})
 
 def verify(request, subject_ID):
     subject = Subject.objects.get(id=subject_ID)
