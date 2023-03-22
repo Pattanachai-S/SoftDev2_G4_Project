8601de6656c04eb41feb370866e647cc023a7efc
Pattanachai-S <69448138+Pattanachai-S@users.noreply.github.com>
2023-03-23 02:42:57 +0700

    remove old test database

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -83,10 +83,6 @@ DATABASES = {
         'TEST': {
             'NAME': 'mytestdatabase.sqlite3',
         },
-    },
-    'test': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db_test.sqlite3',
     }
 }
 
