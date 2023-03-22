24730b3890d2a774a96cde0447fe2c229e8eb40f
Pattanachai-S <69448138+Pattanachai-S@users.noreply.github.com>
2023-03-23 05:32:15 +0700

    up allow host

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0+pd__+t=sg1x$yk9x!h247h)q!_%sh66qep*135pl6z+t^)@y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['BytheWay.pythonanywhere.com']
 
 
 # Application definition
